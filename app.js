e0fdd7cb65454298704723c05b4e649b969b02de
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-05-19 16:28:21 +0200

    df

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 var PORT = process.env.PORT || 5000;
 // *****************************************************************************
+var visitors;
+// *****************************************************************************
+var pg = require("pg");
+var client = new pg.Client(process.env.DATABASE_URL)
+client.connect();
+
+// *****************************************************************************
+function visited (){
+     client.query("SELECT visits FROM data WHERE id = 1",function(err,result){
+         visitors = result;
+         console.log(result);
+     })
+}
+// *****************************************************************************
 app.get('/',function (req,res) {
     res.render('home')
 })
